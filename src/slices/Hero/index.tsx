2cb76326d4c0391a2507589f9c49bdef55580dac
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-09-07 21:35:16 +0530

    gsap scroll trigger animation ✨

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -5,12 +5,13 @@ import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
 import { TextSplitter } from "@/components/TextSplitter";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 /**
  * Props for `Hero`.
@@ -45,6 +46,40 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         opacity: 0,
         y: 10,
         duration: 0.6,
+      });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollTl
+      .fromTo(
+        "body",
+        { backgroundColor: "#FDE047" },
+        {
+          backgroundColor: "#D9F99D",
+          overwrite: "auto",
+        },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 1.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", {
+        y: 20,
+        opacity: 0,
       });
   });
 
