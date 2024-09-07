8012de81b06f21d0deacd9c0925f8b6ad9859f31
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-09-07 20:36:50 +0530

    first gsap animation 🥳

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,9 +1,16 @@
+"use client";
+
+import { asText, Content } from "@prismicio/client";
+import { PrismicNextImage } from "@prismicio/next";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 import { Bounded } from "@/components/Bounded";
 import Button from "@/components/Button";
-import { asText, Content } from "@prismicio/client";
-import { PrismicNextImage, PrismicNextLink } from "@prismicio/next";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
-import { TextSplitter } from "./TextSplitter";
+import { TextSplitter } from "@/components/TextSplitter";
+
+gsap.registerPlugin(useGSAP);
 
 /**
  * Props for `Hero`.
@@ -14,11 +21,38 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl
+      .set(".hero", { opacity: 1 })
+      .from(".hero-header-word", {
+        scale: 3,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.3,
+        stagger: 1,
+      })
+      .from(
+        ".hero-subheading",
+        {
+          opacity: 0,
+          y: 30,
+        },
+        "+=0.8",
+      )
+      .from(".hero-body", {
+        opacity: 0,
+        y: 10,
+        duration: 0.6,
+      });
+  });
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
-      className="hero"
+      className="hero opacity-0"
     >
       <div className="grid">
         <div className="grid h-screen place-items-center">
