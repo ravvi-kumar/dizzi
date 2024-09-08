643eaff2d65f570bce33c4341907b9ac126611a9
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-09-08 13:32:22 +0530

    fix : add @react-three/drei Loader and Suspense

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,32 +1,44 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { EnvironmentCube, View } from "@react-three/drei";
+import { View } from "@react-three/drei";
+// import { Perf } from "r3f-perf";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 
-import FloatingCan from "@/components/FloatingCan";
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader),
+  { ssr: false },
+);
 
 type Props = {};
 
 export default function ViewCanvas({}: Props) {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        overflow: "hidden",
-        pointerEvents: "none",
-        zIndex: 30,
-      }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-      camera={{
-        fov: 30,
-      }}
-    >
-      <View.Port />
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+        camera={{
+          fov: 30,
+        }}
+      >
+        <Suspense fallback={null}>
+          <View.Port />
+        </Suspense>
+        {/* <Perf /> */}
+      </Canvas>
+      <Loader />
+    </>
   );
 }
