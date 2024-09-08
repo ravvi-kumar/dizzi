1a2dd06bbdfc361b3167fe1fd2cfac99719c0a6c
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-09-08 11:35:26 +0530

    changed the fizzi logo to dizzi

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ type Props = {};
 function Header({}: Props) {
   return (
     <header className="-mb-28 flex justify-center py-4">
-      <FizziLogo className="z-10 h-20 cursor-pointer text-sky-800" />
+      <FizziLogo className="z-10 cursor-pointer text-sky-800" />
     </header>
   );
 }
